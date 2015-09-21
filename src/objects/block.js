42e1ed9317f6ed16da54d8202a1bdf9206b71b7a
Nathan Demick <ndemick@gmail.com>
2015-09-21 06:06:26 -0400

    Pass through args to Block obj

--- src/objects/block.js
+++ src/objects/block.js
@@ -2,33 +2,24 @@
 /*globals Arcadia */
 
 var Block = function (options) {
-    var args;
-
     options = options || {};
 
     if (options.type === 'fill') {
-        args = {
-            size: { width: 42, height: 42 },
-            border: '5px black',
-            color: '#665945',
-            type: 'fill'
-        };
+        options.size = { width: 42, height: 42 };
+        options.border = '5px black';
+        options.color =  '#665945';
     } else if (options.type === 'mark') {
-        args = {
-            size: { width: 45, height: 45 },
-            border: '8px black',
-            type: 'mark',
-            path: function (context) {
-                // TODO: Pixel ratio
-                context.moveTo(-this.size.width / 2 + 2, this.size.height / 2 - 2);
-                context.lineTo(this.size.width / 2 - 2, -this.size.height / 2 + 2);
-                context.moveTo(this.size.width / 2 - 2, this.size.height / 2 - 2);
-                context.lineTo(-this.size.width / 2 + 2, -this.size.height / 2 + 2);
-            }
+        options.size = { width: 45, height: 45 };
+        options.border = '8px black';
+        options.path = function (context) {
+            context.moveTo(-this.size.width / 3 * Arcadia.PIXEL_RATIO, this.size.height / 3 * Arcadia.PIXEL_RATIO);
+            context.lineTo(this.size.width / 3 * Arcadia.PIXEL_RATIO, -this.size.height / 3 * Arcadia.PIXEL_RATIO);
+            context.moveTo(this.size.width / 3 * Arcadia.PIXEL_RATIO, this.size.height / 3 * Arcadia.PIXEL_RATIO);
+            context.lineTo(-this.size.width / 3 * Arcadia.PIXEL_RATIO, -this.size.height / 3 * Arcadia.PIXEL_RATIO);
         };
     }
 
-    Arcadia.Shape.call(this, args);
+    Arcadia.Shape.call(this, options);
 };
 
 Block.prototype = new Arcadia.Shape();
