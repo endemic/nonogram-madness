c356457f25e5c2097735c61c5d8601ee586c8054
Nathan Demick <ndemick@gmail.com>
2016-05-02 21:14:31 -0400

    Updates to Android project config

--- cordova/platforms/ios/platform_www/cordova_plugins.js
+++ cordova/platforms/ios/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cc.fovea.cordova.purchase/www/store-ios.js",
+        "id": "cc.fovea.cordova.purchase.InAppPurchase",
+        "pluginId": "cc.fovea.cordova.purchase",
+        "clobbers": [
+            "store"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
         "id": "cordova-plugin-console.console",
         "pluginId": "cordova-plugin-console",
@@ -23,23 +31,15 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
-    },
-    {
-        "file": "plugins/cc.fovea.cordova.purchase/www/store-ios.js",
-        "id": "cc.fovea.cordova.purchase.InAppPurchase",
-        "pluginId": "cc.fovea.cordova.purchase",
-        "clobbers": [
-            "store"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cc.fovea.cordova.purchase": "4.0.0",
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-console": "1.0.3",
-    "cordova-plugin-splashscreen": "2.0.0",
-    "cc.fovea.cordova.purchase": "4.0.0"
+    "cordova-plugin-splashscreen": "2.0.0"
 }
 // BOTTOM OF METADATA
 });
