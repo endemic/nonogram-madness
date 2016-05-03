bfa33c7790c80bc10c2f4a5cc2edbefa556b11ae
Nathan Demick <ndemick@gmail.com>
2016-05-02 22:27:30 -0400

    Re-build Android platform

--- cordova/platforms/ios/www/cordova_plugins.js
+++ cordova/platforms/ios/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cc.fovea.cordova.purchase/www/store-ios.js",
-        "id": "cc.fovea.cordova.purchase.InAppPurchase",
-        "pluginId": "cc.fovea.cordova.purchase",
-        "clobbers": [
-            "store"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
         "id": "cordova-plugin-console.console",
         "pluginId": "cordova-plugin-console",
@@ -31,15 +23,34 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cc.fovea.cordova.purchase": "4.0.0",
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-console": "1.0.3",
-    "cordova-plugin-splashscreen": "2.0.0"
+    "cordova-plugin-splashscreen": "2.0.0",
+    "cordova-plugin-extension": "1.5.1",
+    "cordova-plugin-admobpro": "2.17.0",
+    "cordova-plugin-inappbrowser": "1.4.0"
 }
 // BOTTOM OF METADATA
 });
